02057e4316904a14c6264f3194aba7ffa646aa50
Asmaa-Elkilany <asmaa123amk@gmail.com>
2024-05-30 22:54:28 +0300

    Game is Finished

---
+++ src/snake/GameFrame.java
@@ -0,0 +1,27 @@
+
+package snake;
+
+import java.awt.HeadlessException;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author asmaa
+ */
+public class GameFrame extends JFrame{
+
+    public GameFrame(){
+        
+        this.add(new GamePanel());
+        this.setTitle("Snake");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.pack();
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+        
+        
+    }
+    
+    
+}
---
+++ src/snake/SnakeGame.java
@@ -0,0 +1,17 @@
+
+package snake;
+
+/**
+ *
+ * @author asmaa
+ */
+public class SnakeGame {
+
+    public static void main(String[] args) {
+        
+        new GameFrame();
+        
+        
+    }
+    
+}
